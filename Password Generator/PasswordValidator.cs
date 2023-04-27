a38b51c2de760fcd34314e82ac7d428b10ee3929
unknown <ismayilrha@code.edu.az>
2023-04-27 16:03:24 +0400

    added password generator

---
+++ Password Generator/PasswordValidator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Password_Generator
+{
+    internal class PasswordValidator
+    {
+        private string _password;
+        private int _passowrdLength;
+        public bool isValid ;
+        public PasswordValidator(string password, int passowrdLength)
+        {
+            _password = password;
+            _passowrdLength = passowrdLength;
+        }
+
+
+        public string _Password { 
+            get 
+            {
+                return _password;
+                
+            } 
+            set
+            {
+            
+                if(value.Length != _passowrdLength)
+                {
+                    Console.WriteLine($"$Your password must be contain {_passowrdLength} character");
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                }
+
+                bool isNumber = int.TryParse(value, out int result);
+               
+
+
+                // For Just Numbers
+                if (!isNumber)
+                {
+                    Console.WriteLine("Password must be contain just numbers");
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                }
+
+
+                // For letters
+                //bool isLetter = _password.All(char.IsLetter);
+                //if (!isLetter)
+                //{
+                //    Console.WriteLine("Password must be contain just letters");
+                //    isValid = false;
+                //}
+                //else
+                //{
+                //    isValid = true;
+                //}
+                //
+
+           
+
+
+            }
+        }
+
+      
+
+      
+    }
+}
---
+++ Password Generator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Password_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Length of password :: ");
+
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Password :: ");
+
+            string password = Console.ReadLine();
+
+            PasswordValidator validator = new PasswordValidator(password, length);
+
+            validator._Password = password;
+
+
+
+            if (validator.isValid)
+            {
+                Console.WriteLine("Password Generated ....");
+                Console.WriteLine(password);
+            }
+            else
+            {
+                Console.WriteLine("Try Again");
+            }
+            
+
+
+        }
+    }
+}
